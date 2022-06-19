b5e842285c096f578454550fe786572725be05fd
saqibur <saqibur.rahman98@gmail.com>
2022-06-20 00:19:34 +0600

    Fix: Serializer now handles images safely

--- apps/image/api/v1/serializers.py
+++ apps/image/api/v1/serializers.py
@@ -1,9 +1,21 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    CharField,
+    IntegerField,
+    ModelSerializer,
+)
 
 from apps.image.models import ScrapedImage
 
 
 class ScrapedImageModelSerializer(ModelSerializer):
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.image != "":
+            representation['name'] = instance.image.name
+            representation['width'] = instance.image.width
+            representation['height'] = instance.image.height
+        return representation
+
     class Meta:
         model = ScrapedImage
         exclude = [
